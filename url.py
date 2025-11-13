aa392cbac6314a9e8fcbbb82e53bdb9177a2c958
telescope000 <131180938+telescope000@users.noreply.github.com>
2025-11-13 17:08:10 +0800

    Add presigned URL generation for S3 object
    use url.py to get the presigned_url for upload file to s3

---
+++ url.py
@@ -0,0 +1,9 @@
+import boto3
+
+s3 = boto3.client('s3')
+url = s3.generate_presigned_url(
+    'put_object',
+    Params={'Bucket':'starshop-orders5', 'Key':'test.txt'},
+    ExpiresIn=3600
+)
+print(url)
