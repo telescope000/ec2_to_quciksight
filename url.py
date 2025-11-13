bb75f6a73e18690baf049e3f885d2db88f9dd7c2
telescope000 <131180938+telescope000@users.noreply.github.com>
2025-11-13 18:42:57 +0800

    Update url.py
    modify upload file to orders.csv

--- url.py
+++ url.py
@@ -3,7 +3,7 @@ import boto3
 s3 = boto3.client('s3')
 url = s3.generate_presigned_url(
     'put_object',
-    Params={'Bucket':'starshop-orders5', 'Key':'test.txt'},
+    Params={'Bucket':'starshop-orders5', 'Key':'orders.csv'},
     ExpiresIn=36000
 )
 print(url)
