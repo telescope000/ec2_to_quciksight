4b485ada25129c55bff8eb00716b7cfa94ec720a
telescope000 <131180938+telescope000@users.noreply.github.com>
2025-11-13 17:22:05 +0800

    Increase presigned URL expiration time to 10 hours
    modify expire time from 1 hour to 10 hour

--- url.py
+++ url.py
@@ -4,6 +4,6 @@ s3 = boto3.client('s3')
 url = s3.generate_presigned_url(
     'put_object',
     Params={'Bucket':'starshop-orders5', 'Key':'test.txt'},
-    ExpiresIn=3600
+    ExpiresIn=36000
 )
 print(url)
