895f83162060676572580d7be3ec06c208c57a5c
telescope000 <131180938+telescope000@users.noreply.github.com>
2025-11-13 20:47:47 +0800

    Delete mkcsv.py

--- mkcsv.py
+++
@@ -1,5 +0,0 @@
-import csv
-
-# 建立 orders.csv，並寫入欄位名稱
-with open('orders.csv', mode='w', newline='', encoding='utf-8') as f:
-    csv.writer(f).writerow(['datetime','product','region','qty','price'])
